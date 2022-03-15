2365ef786b3d89f5e415cfc105d619f59a217060
hemakaraveenhansika <hemakaraveenhansika@gmail.com>
2022-03-15 15:54:25 +0530

    client json object

--- src/app/ThreadManager.java
+++ src/app/ThreadManager.java
@@ -1,18 +1,24 @@
 package app;
 
+
 import java.io.BufferedReader;
-import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 
 public class ThreadManager implements Runnable {
 
     private final BufferedReader bufferedReader;
-
+    private final PrintWriter writer;
 
     public ThreadManager(Socket clientSocket) throws IOException {
         bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        writer = new PrintWriter(clientSocket.getOutputStream(), true);
     }
 
     @Override
@@ -20,12 +26,19 @@ public class ThreadManager implements Runnable {
         while (true) {
             System.out.println("Thread started");
             String msg = null;
+            JSONObject client_obj = null;
+            JSONParser parser = new JSONParser();
+
+
             try {
                 msg = bufferedReader.readLine();
-            } catch (IOException e) {
+                client_obj = (JSONObject) parser.parse(msg);
+                writer.println(client_obj.toString());
+            } catch (IOException | ParseException e) {
                 e.printStackTrace();
             }
-            System.out.println(msg);
+            System.out.println("client_obj");
+            System.out.println(client_obj.toString());
         }
     }
 }
--- src/module-info.java
+++ src/module-info.java
@@ -1,3 +1,4 @@
 module distributedChatApp {
-	
+    requires json.simple;
+
 }
