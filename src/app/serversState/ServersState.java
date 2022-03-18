83c0fde10befbcedc811f0a66463913e8a871a0a
Sasmitha Dasanayaka <sasmithadasanayaka96@gmail.com>
2022-03-18 16:10:50 +0530

    Singleton pattern achieved in servers state management

---
+++ src/app/serversState/ServersState.java
@@ -0,0 +1,48 @@
+package app.serversState;
+
+import app.server.Client;
+import app.server.Server;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ServersState {
+    private static ServersState serversStateInstance;
+    private final ConcurrentHashMap<Long, Server> serverThreads = new ConcurrentHashMap<>();
+
+    private ServersState() {
+    }
+
+    public static ServersState getInstance() {
+        if (serversStateInstance == null) {
+            synchronized (ServersState.class) {
+                if (serversStateInstance == null) {
+                    serversStateInstance = new ServersState();
+                }
+            }
+        }
+        return serversStateInstance;
+    }
+
+    public void initializeServer(String serverId, String server_config_path) {
+        try {
+            File file = new File(server_config_path);
+            Scanner myReader = new Scanner(file);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                String[] server_config = data.split(" ");
+                if (server_config[0].equals(serverId)) {
+                    Server server = new Server(server_config[0], server_config[1], Integer.parseInt(server_config[2]), Integer.parseInt(server_config[3]));
+                    Thread serverThread = new Thread(server);
+                    serverThreads.put(serverThread.getId(), server);
+                    serverThread.start();
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+}
