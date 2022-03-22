7d360d5a0d4370f622faa6cf5c3a4276bd757b52
missakaherath <missakaherath96@gmail.com>
2022-03-22 18:59:45 +0530

    adding clients - 2

--- src/app/server/Server.java
+++ src/app/server/Server.java
@@ -87,7 +87,7 @@ public class Server implements Runnable{
         return clientHandlerThreadsMap.get(clientThreadId);
     }
 
-    public static boolean addClient(String newClientId){ //add client to the global list
+    public static boolean addClient(String newClientId){ //adding client to the global list
         Set<String> activeClients = LeaderState.getInstance().getActiveClientsList();
         if(activeClients.contains(newClientId)){ // client already exist
             return false;
